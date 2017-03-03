85495942aca524377adfb3bb6774dac2ec413aeb
Jim Dixon <jddixon@gmail.com>
2017-03-03 10:37:14 -0800

    add configuration files

---
+++ xlnode/__init__.py
@@ -0,0 +1,12 @@
+# xlnode/__init__.py
+
+""" Node library for python XLattice packages. """
+
+__version__ = '0.0.2'
+__version_date__ = '2017-03-03'
+
+__all__ = ['__version__', '__version_date__', 'XLNodeError', ]
+
+
+class XLNodeError(RuntimeError):
+    """ General purpose exception for the package. """
