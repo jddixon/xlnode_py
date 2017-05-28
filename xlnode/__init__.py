fc7537c3600e89c1bab5be33cb22199322acaf09
Jim Dixon <jddixon@gmail.com>
2017-05-28 11:39:36 -0700

    drop dist/* from repos; plus minor edits

--- xlnode/__init__.py
+++ xlnode/__init__.py
@@ -2,8 +2,8 @@
 
 """ Node library for python XLattice packages. """
 
-__version__ = '0.0.3'
-__version_date__ = '2017-03-05'
+__version__ = '0.0.4'
+__version_date__ = '2017-06-16'
 
 __all__ = ['__version__', '__version_date__', 'XLNodeError', ]
 
